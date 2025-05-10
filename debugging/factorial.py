ac9e854e063583a7c634a4ec7429706840f33dcb
Nomen-collab <nomenaratsimba01@gmail.com>
2025-05-11 01:52:07 +0200

    add task 0

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
+
 import sys
 
+
 def factorial(n):
+    """Calculate the factorial of a non-negative integer."""
     result = 1
+
+    if n == 0:
+        return 1
+
     while n > 1:
         result *= n
-        n -= 1  # Ajout de cette ligne pour décrémenter n
+        n -= 1  # Decrement n in each iteration
+
     return result
 
-if len(sys.argv) < 2:
-    print("Usage: python3 factorial.py <entier>")
-    sys.exit(1)
 
-try:
-    num = int(sys.argv[1])
-    if num < 0:
-        print("Erreur : La factorielle n'est pas définie pour les nombres négatifs.")
-        sys.exit(1)
-    f = factorial(num)
-    print(f)
-except ValueError:
-    print("Erreur : L'argument doit être un entier valide.")
-    sys.exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        try:
+            num = int(sys.argv[1])
+            f = factorial(num)
+            print(f)
+        except ValueError:
+            print("Error: Please provide a valid integer argument.")
+    else:
+        print("Error: Please provide an integer argument.")
